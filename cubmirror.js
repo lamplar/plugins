e49dcd47f3cbfd4cf52d77e494c55fbb32b66006
lamplar <lares7234@gmail.com>
2026-09-12 06:12:24 +0500

    Update cubmirror.js

--- cubmirror.js
+++ cubmirror.js
@@ -1,36 +1,70 @@
 (function () {
     'use strict';
 
-    function applyCubFix() {
+    var TARGET_DOMAIN = 'cub.black';
+    var TARGET_URL = 'https://' + TARGET_DOMAIN + '/';
+
+    function applyDeepCubFix() {
         
-        Lampa.Storage.set('cub_domain', 'cub.black');
+        Lampa.Storage.set('cub_domain', TARGET_DOMAIN);
 
         
-        window.cub_domain = 'https://cub.black/';
-
+        if (window.cub_domain) window.cub_domain = TARGET_URL;
         
-        if (window.Lampa && Lampa.CUB) {
-            Lampa.CUB.domain = 'https://cub.black/';
-            if (Array.isArray(Lampa.CUB.mirrors)) {
-                Lampa.CUB.mirrors = ['https://cub.black/'];
+        if (window.Lampa) {
+            
+            if (Lampa.CUB) {
+                Lampa.CUB.domain = TARGET_URL;
+                Lampa.CUB.mirrors = [TARGET_URL];
             }
-        }
+            
+            if (Lampa.Account) {
+                Lampa.Account.url = function (method) {
+                    return TARGET_URL + 'api/' + method;
+                };
+            }
 
-        
-        if (window.Lampa && Lampa.Account) {
-            Lampa.Account.url = function (method) {
-                return 'https://cub.black/api/' + method;
-            };
+            
+            if (Lampa.network && !Lampa.network.cub_patched) {
+                
+                
+                var originalRequest = Lampa.network.request;
+                Lampa.network.request = function (url, method, data, onsuccess, onerror, options) {
+                    if (typeof url === 'string') {
+                        
+                        url = url.replace(/https?:\/\/(cub\.best|cub\.watch|cub\.red|cub\.tv|cub\.pw)\//gi, TARGET_URL);
+                    } else if (url && typeof url.url === 'string') {
+                        url.url = url.url.replace(/https?:\/\/(cub\.best|cub\.watch|cub\.red|cub\.tv|cub\.pw)\//gi, TARGET_URL);
+                    }
+                    return originalRequest.apply(this, arguments);
+                };
+
+                
+                var originalSilent = Lampa.network.silent;
+                if (originalSilent) {
+                    Lampa.network.silent = function (url, onsuccess, onerror) {
+                        if (typeof url === 'string') {
+                            url = url.replace(/https?:\/\/(cub\.best|cub\.watch|cub\.red|cub\.tv|cub\.pw)\//gi, TARGET_URL);
+                        }
+                        return originalSilent.apply(this, arguments);
+                    };
+                }
+
+                Lampa.network.cub_patched = true;
+            }
         }
     }
 
     
-    applyCubFix();
+    applyDeepCubFix();
+
+    
+    
     if (window.appready) {
-        applyCubFix();
+        applyDeepCubFix();
     } else {
         Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') applyCubFix();
+            if (e.type === 'ready') applyDeepCubFix();
         });
     }
 })();
