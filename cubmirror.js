f6dfdad6859b6af985edab1ce5a1d5859c421ac1
lamplar <lares7234@gmail.com>
2026-09-12 07:58:47 +0500

    Update cubmirror.js

--- cubmirror.js
+++ cubmirror.js
@@ -1,61 +1,20 @@
-(function () {
+(function() {
     'use strict';
 
-    function initUltimateCubFix() {
-        // Защита от двойного запуска
-        if (!window.Lampa || !Lampa.network || Lampa.network.cub_black_patched) return;
+    Lampa.Listener.follow('request_before', function(e) {
+        if (e.params && e.params.url) {
+            var url = e.params.url;
 
-        // Функция подмены адресов на лету
-        function fixUrl(urlStr) {
-            if (!urlStr) return urlStr;
-            
-            // 1. Постеры и картинки (TMDB proxy): направляем на cub.best
-            if (/^https?:\/\/(api)?tmdb\.cub\.[a-z]+/i.test(urlStr)) {
-                // Сохраняем начало (tmdb или apitmdb) и меняем только конец на .best
-                return urlStr.replace(/^(https?:\/\/(?:api)?tmdb\.cub\.)[a-z]+/i, '$1best');
+            // 1. Постеры и картинки (TMDB) перехватываем на cub.best
+            if (/^https?:\/\/(api)?tmdb\.cub\.[a-z]+/i.test(url)) {
+                e.params.url = url.replace(/^(https?:\/\/(?:api)?tmdb\.cub\.)[a-z]+/i, '$1best');
             }
-            
-            // 2. Страницы фильмов, сериалов, комментарии и аккаунт: направляем на cub.black
-            if (/^https?:\/\/cub\.[a-z]+/i.test(urlStr)) {
-                return urlStr.replace(/^https?:\/\/cub\.[a-z]+/i, 'https://cub.black');
+            // 2. ТОЛЬКО запросы к API (фильмы, аккаунт, закладки) кидаем на cub.black
+            // Ищем строку вида "cub.что-то/api/"
+            else if (/^https?:\/\/cub\.[a-z]+\/api\//i.test(url)) {
+                e.params.url = url.replace(/^https?:\/\/cub\.[a-z]+/i, 'https://cub.black');
             }
-            
-            return urlStr;
         }
+    });
 
-        // Перехватываем стандартные запросы
-        var originalRequest = Lampa.network.request;
-        Lampa.network.request = function (url, method, data, onsuccess, onerror, options) {
-            if (typeof url === 'string') {
-                url = fixUrl(url);
-            } else if (url && url.url) {
-                url.url = fixUrl(url.url);
-            }
-            return originalRequest.apply(this, arguments);
-        };
-
-        // Перехватываем фоновые запросы (например, отметки о просмотре)
-        var originalSilent = Lampa.network.silent;
-        if (originalSilent) {
-            Lampa.network.silent = function (url, onsuccess, onerror) {
-                if (typeof url === 'string') {
-                    url = fixUrl(url);
-                } else if (url && url.url) {
-                    url.url = fixUrl(url.url);
-                }
-                return originalSilent.apply(this, arguments);
-            };
-        }
-
-        Lampa.network.cub_black_patched = true; // Ставим метку
-    }
-
-    // Запускаем при полной загрузке
-    if (window.appready) {
-        initUltimateCubFix();
-    } else {
-        Lampa.Listener.follow('app', function (e) {
-            if (e.type === 'ready') initUltimateCubFix();
-        });
-    }
 })();
